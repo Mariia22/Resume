749838f14e7fc5df96c7c26bdec3e00e7ef08abd
Maria22 <m.ushakova22@gmail.com>
2020-03-21 21:54:53 +0100

    Add files

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var del = require("del");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task("css", function () {
+  return gulp
+    .src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
+  gulp.watch("source/*.html"), gulp.series("html", "refresh");
+});
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+gulp.task("del", function () {
+  return del("build");
+});
+
+
+gulp.task("html", function () {
+  return gulp
+    .src("source/*.html")
+    .pipe(posthtml([include()]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("minify", () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task(
+  "build",
+  gulp.series("del", "copy", "css", "minify", "html")
+);
+gulp.task("start", gulp.series("build", "server"));
