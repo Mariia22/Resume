3d94f42cc41a858a84a68778abfa1d527af5e478
Maria22 <m.ushakova22@gmail.com>
2020-06-22 22:26:55 +0200

    - Add gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var gulp = require("gulp");
 var sass = require("gulp-sass"), // переводит SASS в CSS
   cssnano = require("gulp-cssnano"), // Минимизация CSS
   autoprefixer = require("gulp-autoprefixer"), // Проставлет вендорные префиксы в CSS для поддержки старых браузеров
+  imagemin = require('gulp-imagemin'), // Сжатие изображение
   concat = require("gulp-concat"), // Объединение файлов - конкатенация
-  rename = require("gulp-rename"); // Переименование файлов
+  rename = require("gulp-rename"), // Переименование файлов
+  watch = require('gulp-watch');
 
 // Копирование файлов HTML в папку dist
 gulp.task("html", function () {
@@ -14,13 +16,22 @@ gulp.task("html", function () {
     .pipe(gulp.dest("dist"));
 });
 
+// Сжимаем картинки
+gulp.task('imgs', function () {
+  return gulp.src("src/images/*.+(jpg|jpeg|png|gif)")
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      interlaced: true
+    }))
+    .pipe(gulp.dest("dist/images"))
+});
 // Объединение, компиляция Sass в CSS, простановка венд. префиксов и дальнейшая минимизация кода
 gulp.task("sass", function () {
   return gulp.src("src/sass/*.sass")
     .pipe(concat('styles.scss'))
     .pipe(sass())
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cssnano())
@@ -30,13 +41,12 @@ gulp.task("sass", function () {
     .pipe(gulp.dest("dist/css"));
 });
 
-// Задача слежения за измененными файлами
+//Задача слежения за измененными файлами
 gulp.task("watch", function () {
-  gulp.watch("src/*.html", ["html"]);
-  gulp.watch("src/sass/*.sass", ["sass"]);
+  gulp.watch("src/*.html", gulp.series("html"));
+  gulp.watch("src/sass/*.sсss", gulp.series("sass"));
+  gulp.watch("src/images/*.+(jpg|jpeg|png|gif)", gulp.series("imgs"));
 });
-
-///// Таски ///////////////////////////////////////
 
 // Запуск тасков по умолчанию
 gulp.task("default", gulp.series("html", "sass", "watch"));
